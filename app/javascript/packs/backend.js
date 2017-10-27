2fb7736b247cf87ca6e9b381d89d5e64fbc81fe1
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-10-27 07:04:46 +0100

    Import base js files

--- app/javascript/packs/backend.js
+++ app/javascript/packs/backend.js
@@ -3,6 +3,50 @@
  #             + CSS File
  #
  ==================================*/
-import '../src/backend.scss'
-import 'images/backend/logos/logo.png'
-import 'images/backend/logos/logo-default-dark.png'
+import '../src/backend.scss';
+import 'images/backend/logos/logo.png';
+import 'images/backend/logos/logo-default-dark.png';
+
+/*==================================
+#       External Dependencies
+==================================*/
+import 'bootstrap';
+
+/*==================================
+#         Local dependencies
+==================================*/
+import Router from 'javascripts/backend/util/Router';
+
+/*==================================
+ #              Pages
+ ==================================*/
+import dashboard from 'javascripts/backend/routes/dashboard';
+
+/*==================================
+ #              Layout
+ ==================================*/
+import plugins from 'javascripts/backend/layout/plugins';
+import uis from 'javascripts/backend/layout/uis';
+
+
+/*==================================
+ #    Populate Router instance
+ #         with DOM routes
+ ==================================*/
+const routes = new Router({
+  // Layouts
+  plugins,
+  uis,
+  // Pages
+  dashboard
+});
+
+/*==================================
+ #             Turbolinks
+ ==================================*/
+import Turbolinks from 'turbolinks';
+Turbolinks.start();
+
+document.addEventListener('turbolinks:load', function () {
+  return routes.loadEvents();
+});
