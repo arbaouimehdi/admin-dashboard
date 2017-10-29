1626cdf197e0e50933f27b62b902c97ce8e26193
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-10-29 11:09:41 +0000

    Add Sidebar Menu javascript

--- app/assets/javascripts/backend/layout/uis.js
+++ app/assets/javascripts/backend/layout/uis.js
@@ -30,5 +30,61 @@ export default {
 
     })
 
+  },
+
+  /**
+   * Aside Menu
+   *
+   * @param selector
+   * @param targetState
+   */
+  asideMenu({selector, targetState}){
+
+    let sub_menu,
+        expanded_list,
+        list;
+
+    $(selector).on('click', function(){
+
+      sub_menu = $(this).next();
+      list = $(this).parent();
+      expanded_list = $('.m-menu-item.m-menu-item-submenu').not('.m-menu-item-expanded');
+
+      //
+      //
+      // Open the Menu
+      if (!list.hasClass(targetState)){
+
+        // Close all Sub Menus except the Current and the Expanded Ones
+        expanded_list.find('.m-menu-submenu').slideUp(function(){
+          expanded_list.not(sub_menu).removeClass(targetState);
+        });
+
+        // Open the current Sub Menu
+        sub_menu.slideDown(function(){
+          $(this).parent().addClass(targetState);
+        });
+
+        console.log('open');
+
+      }
+
+      //
+      //
+      // Close the Sub Menu
+      else {
+
+        // Close the current Sub Menu
+        sub_menu.slideUp(function(){
+          $(this).parent().removeClass(targetState);
+        });
+
+        console.log('closed');
+
+      }
+
+    });
+
   }
+
 }
--- app/assets/javascripts/backend/routes/dashboard.js
+++ app/assets/javascripts/backend/routes/dashboard.js
@@ -10,13 +10,15 @@ import ui from '../layout/uis'
 
 export default {
   init() {
-
     ui.asideToggle({
       selector: '#aside-toggle',
       target: 'body',
       targetState: 'm-brand-minimize m-aside-left-minimize',
       togglerState: 'm-brand-toggler-active',
     });
-
+    ui.asideMenu({
+      selector: '.m-menu-link',
+      targetState: 'm-menu-item-open'
+    });
   }
 };
