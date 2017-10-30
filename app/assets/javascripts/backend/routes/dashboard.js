2baf707b1f31dc8a0f201f1bca0a460411f77520
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-10-30 07:13:49 +0000

    Add Aside Menu Hover

--- app/assets/javascripts/backend/routes/dashboard.js
+++ app/assets/javascripts/backend/routes/dashboard.js
@@ -10,16 +10,24 @@ import ui from '../layout/uis'
 
 export default {
   init() {
+
     ui.asideToggle({
       selector: '#aside-toggle',
       target: 'body',
       targetState: 'm-brand-minimize m-aside-left-minimize',
       togglerState: 'm-brand-toggler-active',
     });
+
     ui.asideMenu({
       targetMenu: '.m-aside-menu',
       selector: '.m-menu-link',
       targetState: 'm-menu-item-open'
     });
+
+    ui.asideMenuHover({
+      targetMenu: '.m-aside-menu',
+      selector: '.m-menu-nav > .m-menu-item > .m-menu-link'
+    });
+
   }
 };
